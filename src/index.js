4f5e0d63b6f08238176bd532d9b12b112f5eca26
Serhii Chernysh <srg.chernysh@gmail.com>
2019-09-23 11:03:11 +0300

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -37,8 +37,54 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+function createArrFromStr(str) {
+    let arr = [];
+    let lastPos = str.length;
+    for (let i = 0; i < lastPos; i += 10) {
+        arr.push(str.slice(i, i+10));
+    }
+    return arr;
+}
+
+function clearExtraZeroFromItem(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i][0] === '*') continue;
+        arr[i] = arr[i].slice(arr[i].indexOf('1'));
+    }
+    return arr;
+}
+
+function decodeItemToMorseCode(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let str = '';
+        for (let j = 0; j < arr[i].length; j += 2) {
+            if (arr[i].slice(j, j+2) === '10') {
+                str += '.';
+            } else if (arr[i].slice(j, j+2) === '11') {
+                str += '-';
+            } else {
+                str = ' ';
+            }
+        }
+        arr[i] = str;
+    }
+    return arr;
+}
+
 function decode(expr) {
     // write your solution here
+    let decodedExpr = '';
+    let codeList = createArrFromStr(expr);
+    clearExtraZeroFromItem(codeList);
+    decodeItemToMorseCode(codeList);
+    for (let i = 0; i < codeList.length; i++) {
+        if (codeList[i] === ' ') {
+            decodedExpr += codeList[i];
+        } else {
+            decodedExpr += MORSE_TABLE[codeList[i]];
+        }
+    }
+    return decodedExpr;
 }
 
 module.exports = {
